89ebab4f6f23f9d6f8dc8fcf247ab9156071b738
Vivek Shukla <vivekshuklahere@gmail.com>
2023-12-03 16:05:53 +0530

    changed folder structure

---
+++ app/01-colors/page.tsx
@@ -0,0 +1,15 @@
+const page = () => {
+  return (
+    <div>
+      <div className="border-4 border-yellow-500">
+        <h1 className="text-3xl font-bold underline text-green-500">
+          Hello World
+        </h1>
+        <h1 className="text-3xl font-bold bg-yellow-100 text-white">
+          Hello World
+        </h1>
+      </div>
+    </div>
+  );
+}
+export default page
--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div className="border-4 border-yellow-500">
-        <h1 className="text-3xl font-bold underline text-green-500">
-          Hello World
-        </h1>
-        <h1 className="text-3xl font-bold bg-yellow-100 text-white">
-          Hello World
-        </h1>
-      </div>
+      <h1>TailwindCSS</h1>
     </div>
   );
 }
